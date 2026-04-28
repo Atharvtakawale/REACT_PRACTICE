fcc2d1487c7b510e172a13a335287af200398e53
Atharv-Takawale <atharv.takawale@epps-erp.in>
2026-04-28 10:58:22 +0530

    01_folder_done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+     <div className="flex items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
